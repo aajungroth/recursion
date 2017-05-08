a70f83f4b9e0d0287667736dff4abaca91ad0883
aajungroth <aajungroth@ucdavis.edu>
2017-05-07 17:42:12 -0700

    Added pseudo code

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
+  return $(document).ready(function() {
+    var body = $(document).find(body);
+    var nodeList = [];
+    var getClassNodes = function(node, className) {
+      //iterate through the children
+        //recurse on the children
+      //if the this node has the class
+        //add it to the node list
+    };
+
+    getClassNodes(body, className);
+    return nodeList;
+  });
 };
